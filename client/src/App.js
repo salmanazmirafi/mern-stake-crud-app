370b300e31de75728f664665240fa4a4e5394359
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-16 19:19:33 +0600

    bootstrap setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="App">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
